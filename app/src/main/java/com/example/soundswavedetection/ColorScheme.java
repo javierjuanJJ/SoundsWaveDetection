a022b889c23becf4a548035c4022569cbb46057b
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-09-11 10:28:24 +0200

    Part 2: Commit 1

--- app/src/main/java/com/example/soundswavedetection/ColorScheme.java
+++ app/src/main/java/com/example/soundswavedetection/ColorScheme.java
@@ -1,5 +1,28 @@
 package com.example.soundswavedetection;
 
+import android.graphics.Color;
+import android.graphics.Paint;
+
 class ColorScheme {
+
+
+    private static final int COLOR_CHANGE_FRAME_RATE = 20;
+    private static int colorChangeFrameSea = 0;
+
+    public Paint CanvasPoint, CirclePaint, LinePaint;
+
+    public ColorScheme (){
+        LinePaint = new Paint();
+        LinePaint.setStrokeWidth(1);
+        LinePaint.setColor(Color.GREEN);
+
+        CirclePaint = new Paint();
+        CirclePaint.setStrokeWidth(5);
+        CirclePaint.setColor(Color.GREEN);
+
+        CanvasPoint = new Paint();
+        CanvasPoint.setStrokeWidth(5);
+        CanvasPoint.setColor(Color.WHITE);
+    }
 
 }
