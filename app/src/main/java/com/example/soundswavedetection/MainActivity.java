27305a7d20e36bc9bd29205ff079462baa1ca5db
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-09-14 10:18:55 +0200

    Part 3: Commit 2

--- app/src/main/java/com/example/soundswavedetection/MainActivity.java
+++ app/src/main/java/com/example/soundswavedetection/MainActivity.java
@@ -8,11 +8,15 @@ import androidx.core.content.ContextCompat;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.media.MediaRecorder;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     public static final int APP_PERMISSIONS_RECORD_AUDIO = 1;
@@ -21,6 +25,26 @@ public class MainActivity extends AppCompatActivity {
     private MediaRecorder recorder;
     private Handler handler = new Handler();
     TextView txtName;
+    private Runnable updater = new Runnable() {
+        @Override
+        public void run() {
+            handler.postDelayed(this, 50);
+            if (recorder != null && screenVisualization != null) {
+                int maxAmplitude = 0;
+                try {
+                    maxAmplitude = recorder.getMaxAmplitude();
+                    txtName.setVisibility(View.INVISIBLE);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                if (maxAmplitude > 0)
+                {
+                    screenVisualization.addAmplitude(maxAmplitude);
+                }
+            }
+        }
+    };
 
 
     @Override
@@ -33,12 +57,14 @@ public class MainActivity extends AppCompatActivity {
         screenVisualization = findViewById(R.id.svisualization);
         txtName = findViewById(R.id.labelName);
 
-        screenVisualization.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startVoiceListening();
-            }
-        });
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.N) {
+            screenVisualization.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startVoiceListening();
+                }
+            });
+        }
 
     }
 
@@ -86,6 +112,63 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void startVoiceListening() {
+        if(chaeckPermissions()){
+            recorder = new MediaRecorder();
+            recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+            File file = null;
+
+            try {
+                file = File.createTempFile("prefix", ".extension", getApplicationContext().getCacheDir());
+                if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+                    recorder.setOutputFile(file);
+                }
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+            try {
+                Toast.makeText(this, "Ready to list to voice", Toast.LENGTH_SHORT).show();
+
+                recorder.prepare();
+
+                Toast.makeText(this, "Trying listening to voice", Toast.LENGTH_SHORT).show();
+
+                recorder.start();
+
+                Toast.makeText(this, "Started listening to voice", Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+        }
+
+        else {
+            RequestPermissions();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(updater);
+
+        if (recorder != null) {
+            recorder.stop();
+            recorder.reset();
+            recorder.release();
+            recorder = null;
+        }
+
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        handler.post(updater);
 
     }
 }
